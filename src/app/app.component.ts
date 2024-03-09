75e551805d982492cb07fd4f96e182f595dfa380
Carlos-Felipe Rodriguez <feliperod0519@hotmail.com>
2024-03-09 11:11:36 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'json-read';
+
+  studentData: any;
+
+  url:string = './assets/users.json';
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get(this.url).subscribe(res=>{
+      this.studentData = res;
+    })
+  }
 }
